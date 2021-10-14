f778fc140654ef3724f0458915fd732db6188eff
Brigham Michaelis <brigham.michaelis@gmail.com>
2021-10-13 22:14:03 -0600

    Make input work and show all reqd changes.

--- ExtendedInput/ExtendedInput/Game1.cs
+++ ExtendedInput/ExtendedInput/Game1.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using MonoGame.Extended.Input.InputListeners;
+using System;
 
 namespace ExtendedInput
 {
@@ -9,16 +11,59 @@ namespace ExtendedInput
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
+        // First we declare some Listener Objects
+        private readonly GamePadListener _gamePadListener1;
+        private readonly GamePadListener _gamePadListener2;
+        private readonly KeyboardListener _keyboardListener;
+        private readonly MouseListener _mouseListener;
+
+        // Then we declare some listner settings things that can
+        // be used to instantiate the listener objects mentioned above.
+        private GamePadListenerSettings gplsobj1;
+        private GamePadListenerSettings gplsobj2;
+        private KeyboardListenerSettings kblsobj;
+        private MouseListenerSettings mlsobj;
+
+
+
+
+
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
+
+
+
+            gplsobj1 = new GamePadListenerSettings(PlayerIndex.One);
+            gplsobj2 = new GamePadListenerSettings(PlayerIndex.Two);
+            kblsobj = new KeyboardListenerSettings();
+            mlsobj = new MouseListenerSettings();
+            _gamePadListener1 = new GamePadListener(gplsobj1);
+            _gamePadListener2 = new GamePadListener(gplsobj2);
+            _keyboardListener = new KeyboardListener(kblsobj);
+            _mouseListener = new MouseListener(mlsobj);
+            Components.Add(new InputListenerComponent(this, _gamePadListener1));
+            Components.Add(new InputListenerComponent(this, _gamePadListener2));
+            Components.Add(new InputListenerComponent(this, _keyboardListener));
+            Components.Add(new InputListenerComponent(this, _mouseListener));
+
+
+        }
+
+        public void handleButtonDownInput(Object sender, GamePadEventArgs args)
+        {
+            System.Console.WriteLine(args.Button.ToString());
         }
 
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+
+            _gamePadListener1.ButtonDown += handleButtonDownInput;
+            // _gamePadListener1.ButtonUp += handle game pad button up input
+            // How would you bind four smash bros to 4 controllers?
 
             base.Initialize();
         }
